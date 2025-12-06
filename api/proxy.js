6c9dbe9a4dab69e1a1514dbadf5b2926d8d0c5f6
X-jelly18 <elitegamer128k@gmail.com>
2025-12-06 10:22:26 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,30 +1,30 @@
-const https = require('https');
+const http = require('http');
 
 module.exports = async (req, res) => {
-const backendHost = 'gk.33ffkiyotaka222.ggff.net';
-const backendPath = req.url;
+  const backendHost = 'gk.33ffkiyotaka222.ggff.net';
+  const backendPath = req.url;
 
-const options = {
-hostname: backendHost,
-port: 80,
-path: backendPath,
-method: req.method,
-headers: { ...req.headers, host: backendHost },
-};
+  const options = {
+    hostname: backendHost,
+    port: 80, // changed from 443 to 80
+    path: backendPath,
+    method: req.method,
+    headers: { ...req.headers, host: backendHost },
+  };
 
-const backendReq = https.request(options, backendRes => {
-res.writeHead(backendRes.statusCode, backendRes.headers);
-backendRes.pipe(res, { end: true });
-});
+  const backendReq = http.request(options, backendRes => {
+    res.writeHead(backendRes.statusCode, backendRes.headers);
+    backendRes.pipe(res, { end: true });
+  });
 
-backendReq.on('error', err => {
-console.error('Backend request error:', err);
-res.status(502).send('Bad Gateway');
-});
+  backendReq.on('error', err => {
+    console.error('Backend request error:', err);
+    res.status(502).send('Bad Gateway');
+  });
 
-if (req.method !== 'GET' && req.method !== 'HEAD') {
-req.pipe(backendReq, { end: true });
-} else {
-backendReq.end();
-}
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    req.pipe(backendReq, { end: true });
+  } else {
+    backendReq.end();
+  }
 };
