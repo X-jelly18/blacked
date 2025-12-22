aa887974106064f1cb9a13275c44b0affbf80bdb
X-jelly18 <elitegamer128k@gmail.com>
2025-12-22 20:27:32 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,30 +1,18 @@
-const https = require('https');
-
 module.exports = async (req, res) => {
-const backendHost = 'gsa.ayanakojivps.shop';
-const backendPath = req.url;
+  const backendHost = 'news.ayanakojixxx.shop';
+  const backendUrl = `https://${backendHost}${req.url}`;
 
-const options = {
-hostname: backendHost,
-port: 443,
-path: backendPath,
-method: req.method,
-headers: { ...req.headers, host: backendHost },
-};
+  // Use 307 to preserve method & body (VERY IMPORTANT)
+  res.writeHead(307, {
+    Location: backendUrl,
 
-const backendReq = https.request(options, backendRes => {
-res.writeHead(backendRes.statusCode, backendRes.headers);
-backendRes.pipe(res, { end: true });
-});
+    // Cache redirect at CDN + browser
+    // Client will skip Vercel on future requests
+    'Cache-Control': 'public, max-age=86400, s-maxage=86400',
 
-backendReq.on('error', err => {
-console.error('Backend request error:', err);
-res.status(502).send('Bad Gateway');
-});
+    // Optional: security hardening
+    'Referrer-Policy': 'no-referrer',
+  });
 
-if (req.method !== 'GET' && req.method !== 'HEAD') {
-req.pipe(backendReq, { end: true });
-} else {
-backendReq.end();
-}
+  res.end();
 };
